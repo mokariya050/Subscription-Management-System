13680f48d76711024b7a25d7b3a37eba4cae0128
Heet Radadiya <74094994+Heet06@users.noreply.github.com>
2026-04-04 13:21:58 +0530

    Serve provided HTML screens in Vite without visual changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,28 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
-import SignUpPage from './pages/SignUpPage'
-import ResetPasswordPage from './pages/ResetPasswordPage'
-import SplashScreen from './pages/SplashScreen'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SplashScreen />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
-      </Routes>
-    </Router>
-  )
+function HtmlScreen({ src, title }) {
+    return (
+        <iframe
+            title={title}
+            src={src}
+            className="screen-frame"
+            loading="eager"
+            referrerPolicy="no-referrer"
+            sandbox="allow-scripts allow-forms allow-same-origin allow-popups"
+        />
+    )
 }
 
-export default App
+export default function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate to="/splash/loading" replace />} />
+            <Route path="/splash/loading" element={<HtmlScreen title="Splash Loading" src="/screens/splash_screen_initial_load/code.html" />} />
+            <Route path="/splash/success" element={<HtmlScreen title="Splash Success" src="/screens/splash_screen_success_state/code.html" />} />
+            <Route path="/splash/error" element={<HtmlScreen title="Splash Error" src="/screens/splash_screen_error_state/code.html" />} />
+            <Route path="/login" element={<HtmlScreen title="Login" src="/screens/login_subsync/code.html" />} />
+            <Route path="/signup" element={<HtmlScreen title="Sign Up" src="/screens/sign_up_subsync/code.html" />} />
+            <Route path="/reset-password" element={<HtmlScreen title="Reset Password" src="/screens/reset_password_subsync/code.html" />} />
+        </Routes>
+    )
+}
